ec31cdd5d7e3423a76a4d982d07c14c7e02f2328
Kelvin.Ye <testmankelvin@163.com>
2021-05-26 18:05:11 +0800

    🐞 fix: fix bug

--- sendanywhere/common/exceptions.py
+++ sendanywhere/common/exceptions.py
@@ -3,9 +3,6 @@
 # @File    : exception.py
 # @Time    : 2019/3/15 10:48
 # @Author  : Kelvin.Ye
-from sendanywhere.utils.log_util import get_logger
-
-log = get_logger(__name__)
 
 
 class InvalidVariableException(Exception):
