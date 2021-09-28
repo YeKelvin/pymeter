26bce22aefe2a1858a85d067921415cd50efc2a6
Kelvin.Ye <testmankelvin@163.com>
2021-09-28 16:38:56 +0800

    chore: 优化json异常时的日志输出

--- pymeter/utils/json_util.py
+++ pymeter/utils/json_util.py
@@ -4,29 +4,38 @@
 # @Time    : 2020/2/21 11:14
 # @Author  : Kelvin.Ye
 import random
-from collections.abc import Iterable
+from collections.abc import Sequence
 
 import orjson
 from jsonpath import jsonpath
+from orjson import JSONDecodeError
 
 
 def to_json(obj: dict or list) -> str:
     """序列化"""
-    return orjson.dumps(obj).decode('utf8')
+    try:
+        return orjson.dumps(obj).decode('utf8')
+    except TypeError as e:
+        e.args = e.args + (f'obj:[ {obj} ]',)
+        raise e
 
 
-def from_json(json: str) -> any:
+def from_json(val):
     """反序列化"""
-    return orjson.loads(json)
+    try:
+        return orjson.loads(val)
+    except JSONDecodeError as e:
+        e.args = e.args + (f'value:[ {val} ]',)
+        raise e
 
 
 def json_path(val, xpath, choice=False, index=None):
     results = jsonpath(from_json(val), xpath)
     if len(results) == 1:
         result = results[0]
-        if choice and isinstance(result, Iterable):
+        if choice and isinstance(result, Sequence):
             return random.choice(result)
-        elif index is not None and isinstance(result, Iterable):
+        elif index is not None and isinstance(result, Sequence):
             return result[index]
         else:
             return result
