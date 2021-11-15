3b1220c5ed3b295f247e6662227c7377562f1cd0
Kelvin.Ye <testmankelvin@163.com>
2021-11-15 12:30:01 +0800

    ✨ fix: 修正warning

--- pymeter/controls/controller.py
+++ pymeter/controls/controller.py
@@ -12,40 +12,37 @@ from pymeter.groups.context import ContextService
 from pymeter.samplers.sampler import Sampler
 
 
-class Controller(TestElement, metaclass=ABCMeta):
+class Controller(TestElement):
 
-    @abstractmethod
     @property
     def done(self):
         """是否已完成"""
         raise NotImplementedError
 
-    @abstractmethod
     def next(self) -> Sampler:
         """返回下一个 Sampler，没有下一个时返回 None"""
         raise NotImplementedError
 
-    @abstractmethod
     def initialize(self):
         """在迭代开始时调用以初始化控制器"""
         raise NotImplementedError
 
-    @abstractmethod
     def trigger_end_of_loop(self):
         """在控制器上触发循环结束条件"""
         raise NotImplementedError
 
-    @abstractmethod
     def add_iteration_listener(self, listener):
         raise NotImplementedError
 
-    @abstractmethod
     def remove_iteration_listener(self, listener):
         raise NotImplementedError
 
 
 class IteratingController(LoopIterationListener, metaclass=ABCMeta):
     """迭代控制器"""
+
+    PREFIX = '__pm__'
+    SUFFIX = '__idx__'
 
     @abstractmethod
     def start_next_loop(self) -> None:
@@ -60,4 +57,4 @@ class IteratingController(LoopIterationListener, metaclass=ABCMeta):
     def update_iteration_index(self, name, iter_count) -> None:
         variables = ContextService.get_context().variables
         if variables:
-            variables.put(f'__pm__.{name}.__idx__', iter_count)
+            variables.put(f'{self.PREFIX}.{name}.{self.SUFFIX}', iter_count)
