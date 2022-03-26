1c72b1809027a857f843a1721fbadc4402fbc707
Kelvin.Ye <testmankelvin@163.com>
2022-03-26 20:59:57 +0800

    ✨ feat: json decode error时的提示信息

--- pymeter/utils/json_util.py
+++ pymeter/utils/json_util.py
@@ -4,11 +4,16 @@
 # @Time    : 2020/2/21 11:14
 # @Author  : Kelvin.Ye
 import random
+import re
 from collections.abc import Sequence
 
 import orjson
 from jsonpath import jsonpath
 from orjson import JSONDecodeError
+
+
+DECODE_ERROR_PATTERN = re.compile('line [\d]+ column [\d]+ \(char [\d]+\)$')
+NUMBER_PATTERN = re.compile('[\d]+')
 
 
 def to_json(obj: dict or list) -> str:
@@ -25,8 +30,36 @@ def from_json(val):
     try:
         return orjson.loads(val)
     except JSONDecodeError as e:
-        e.args = e.args + (f'value: {val}',)
-        raise e
+        match = DECODE_ERROR_PATTERN.search(e.args[0])
+        if match:
+            position_msg = match.group()
+            position = NUMBER_PATTERN.findall(position_msg)
+            line = int(position[0])
+            # column = int(position[1])
+            ch = int(position[2])
+            lines = val.split('\n')
+            line_count = len(lines)
+            char_count = len(val)
+            if line == 1:  # 第一行报错，直接用 char 定位
+                if char_count > ch + 10:
+                    if ch > 10:
+                        msg = val[ch -10: ch+10]
+                    else:
+                        msg = val
+                else:
+                    if ch > 10:
+                        msg = val[ch -10]
+                    else:
+                        msg = val
+            else:
+                if line_count > line:
+                    msg = ''.join(lines[line -2: line + 1])
+                else:
+                    msg = ''.join(lines[line -2:])
+            e.args = (f'{e.args[0]}\n--->{msg}',)
+            raise e
+        else:
+            raise e
 
 
 def json_path(val, xpath, choice=False, index=None):
