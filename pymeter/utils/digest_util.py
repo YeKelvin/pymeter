14da359e794bc63831c93cd0cbd4b88d9c01dea3
Kelvin.Ye <testmankelvin@163.com>
2023-12-21 10:55:18 +0800

    ✨ feat: 优化digest加密

--- pymeter/utils/digest_util.py
+++ pymeter/utils/digest_util.py
@@ -2,6 +2,7 @@
 # @File    : digest_util.py
 # @Time    : 2023-06-14 15:44:41
 # @Author  : Kelvin.Ye
+import base64
 import hashlib
 import hmac
 from hashlib import sha256
@@ -11,5 +12,21 @@ def md5(data, charset='utf-8'):
     return hashlib.md5(data.encode(encoding=charset)).hexdigest()
 
 
-def hmac_sha256(data, key, charset='utf-8'):
-    return hmac.new(key.encode(charset), data.encode(charset), digestmod=sha256).hexdigest().upper()
+def hmac_sha256(data, key=None, charset='utf-8') -> bytes:
+    if key:
+        key = key.encode(charset)
+    return hmac.new(key, data.encode(charset), digestmod=sha256).digest()
+
+
+def hmac_sha256_hex(data, key=None, charset='utf-8') -> str:
+    if key:
+        key = key.encode(charset)
+    return hmac.new(key, data.encode(charset), digestmod=sha256).hexdigest().upper()
+
+
+def hmac_sha256_base64(data, key=None, charset='utf-8') -> str:
+    if key:
+        key = key.encode(charset)
+    return base64.b64encode(
+        hmac.new(key, data.encode(charset), digestmod=sha256).digest()
+    ).decode(charset)
